1d6cb1602ef19ea98b1d578f3ea2528d2739baa8
Bernardo Augusto Pereira Lopes <164011941+BernardoApl@users.noreply.github.com>
2024-12-04 20:24:25 -0300

    Create PesquisaPassageiro_test.cpp

---
+++ test/PesquisaPassageiro_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "passageiro.h"  
+
+using namespace std;
+
+// Teste de busca de passageiro
+TEST(PesquisaTest, BuscaPassageiro) {
+    // Criar um passageiro
+    Passageiro passageiro;
+    passageiro.codigo = 1;
+    passageiro.nome = "Carlos Almeida";
+    passageiro.telefone = "123456789";
+    passageiro.documento = "12345678901";
+    passageiro.email = "carlos@email.com";
+
+    salvarPassageiro(passageiro);
+
+    // Realizar a busca
+    Passageiro encontrado = buscaPassageiro(1);
+
+    // Verificar se o passageiro foi encontrado corretamente
+    EXPECT_EQ(encontrado.codigo, 1);
+    EXPECT_EQ(encontrado.nome, "Carlos Almeida");
+    EXPECT_EQ(encontrado.telefone, "123456789");
+    EXPECT_EQ(encontrado.documento, "12345678901");
+    EXPECT_EQ(encontrado.email, "carlos@email.com");
+}
